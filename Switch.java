2d93dd3d3f0f86007dc35a1a6135d8d5a6107c73
Godwin <godwinmail6@gmail.com>
2023-10-03 15:27:19 +0200

    Scanner closed. The switch statement has been created and tested. At this point, the user input converts to a string if it meets the requirements asked.

--- Switch.java
+++ Switch.java
@@ -3,6 +3,35 @@ public class Switch {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+
+
+        System.out.println("Type in num: ");
+        int takeUserInput = Integer.parseInt(input.nextLine());
+
+        switch (takeUserInput)
+        {
+            case 0: {
+                System.out.println("abc");
+                break;
+            }
+            case 1: {
+                System.out.println("def");
+                break;
+            }
+            case 2: {
+                System.out.println("ghi");
+                break;
+            }
+            case 3: {
+                System.out.println("jkl");
+                break;
+            }
+            default:
+                System.out.println("Error. Please confirm input");
+
+        }
         input.close();
     }
+
+
 }
