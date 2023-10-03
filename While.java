f227523db1478639b66515652fba06c4648b5c00
Godwin <godwinmail6@gmail.com>
2023-10-03 16:48:23 +0200

    commit
    commiting the work I have done so far

--- While.java
+++ While.java
@@ -3,9 +3,34 @@ import java.util.Scanner;
 public class While {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        /*Scanner input = new Scanner(System.in);
         System.out.println("Enter new word: ");
         String userInput = input.nextLine();
-        input.close();
+
+        String[] splitStr = userInput.split(",", userInput.length());
+        String letter = "q";
+
+
+       while(splitStr){
+           for(int i = 0; i<splitStr.length; i++){
+               if(letter){
+                   System.out.println("Enter new word: ");
+               }
+           }
+       }
+
+
+        input.close();*/
+
     }
+   /* public static String[] strPlitter(String word)
+    {
+        while(word)
+        {
+            String[] splitString = word.split(",", word.length());
+            return splitString;
+        }
+
+        return new String[0];
+    }*/
 }
