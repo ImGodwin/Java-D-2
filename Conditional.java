afe8c6f887533b6fa008a8a71371e72ce9430f62
Godwin <godwinmail6@gmail.com>
2023-10-03 14:23:13 +0200

    Main created, The first conditional statement is completed

---
+++ Conditional.java
@@ -0,0 +1,15 @@
+public class Conditional {
+    public static void main(String[] args) {
+
+        String str = "Hippopotamus";
+        int strLength = str.length();
+
+        if(strLength % 2 == 0)
+        {
+            System.out.println(true);
+        }else
+        {
+            System.out.println(false);
+        }
+    }
+}
