d19616cfd5a37e02e55b02ced0c333feaa4b6131
Godwin <godwinmail6@gmail.com>
2023-10-03 15:33:19 +0200

    Scanner imported, user can type in new words

--- While.java
+++ While.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class While {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter new word: ");
+        String userInput = input.nextLine();
+        input.close();
     }
 }
