6cb090ecb7f1c8c01b81462113264c55f2caba52
Godwin <godwinmail6@gmail.com>
2023-10-03 19:01:30 +0200

    added more functions
    The functions were updated and so far, everything is working

--- ForLoop.java
+++ ForLoop.java
@@ -2,21 +2,16 @@ import java.util.Scanner;
 
 public class ForLoop {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+      Scanner input = new Scanner(System.in);
 
 
         System.out.println("Type in num: ");
         int number = Integer.parseInt(input.nextLine());
-
-        System.out.println(numToCheck(number));
         input.close();
-    }
-    public static int numToCheck(int num)
-    {
-        int numResult = 0;
-        for (int i = num; i > 0; i--){
-            numResult = num[i];
+
+        for(int i = number; i>= 0; i--)
+        {
+             System.out.println(i);
         }
-        return numResult;
     }
 }
