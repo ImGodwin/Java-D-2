b6adc2648103610c2d364ed534a974caf8ef715f
Godwin <godwinmail6@gmail.com>
2023-10-03 14:48:32 +0200

    second exercise completed. The leap year exercise was returns the appropriate output

--- Conditional.java
+++ Conditional.java
@@ -1,10 +1,21 @@
 public class Conditional {
     public static void main(String[] args) {
 
-        String str = "Hippopotamus";
+        /*String str = "Hippopotamus";
         int strLength = str.length();
 
         if(strLength % 2 == 0)
+        {
+            System.out.println(true);
+        }else
+        {
+            System.out.println(false);
+        }*/
+
+        int year = 2024;
+
+
+        if(year % 4 == 0)
         {
             System.out.println(true);
         }else
